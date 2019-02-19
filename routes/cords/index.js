37dddf44e0ec4b84b5419d4cb4024a4f84d0af11
eevabec <evan.bechtol@ericsson.com>
2019-02-19 15:53:25 -0600

    Retrieve Cord By ID
    - Add route to retrieve a cord by its ID

--- routes/cords/index.js
+++ routes/cords/index.js
@@ -1,6 +1,7 @@
 const express = require( "express" );
 const Cords   = require( "../../models/Cords" );
 const qUtil   = require( "../../controllers/queryUtil" );
+const resUtil = require( "../../controllers/responseUtil" );
 
 let router = express.Router();
 
@@ -8,16 +9,29 @@ router.get( "/", ( req, res ) => {
   const queryStrings = qUtil.getDbQueryStrings( req.query );
 
   Cords
-      .find({})
-      .sort(queryStrings.sort)
-      .limit(queryStrings.limit)
-      .exec(function(err, results) {
-        if (err) {
-          return res.status(500).send(err);
+      .find( {} )
+      .sort( queryStrings.sort )
+      .limit( queryStrings.limit )
+      .exec( function ( err, results ) {
+        if ( err ) {
+          return res.status( 500 ).send( resUtil.sendError( err ) );
         }
 
-        return res.send(results);
-      });
+        return res.send( resUtil.sendSuccess( results ) );
+      } );
+} );
+
+router.get( "/:id", ( req, res ) => {
+  const id = req.query.id || req.params.id || null;
+  Cords
+      .findOne( { _id: id } )
+      .exec( function ( err, results ) {
+        if ( err ) {
+          return res.status( 500 ).send( resUtil.sendError( err ) );
+        }
+
+        return res.send( resUtil.sendSuccess( results ) );
+      } );
 } );
 
 module.exports = router;
