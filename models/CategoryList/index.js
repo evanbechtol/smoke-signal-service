3d32567907a6fbb20c43fabe3460ee001b0087d3
Manoj <manoj.kumart@lnttechservices.com>
2019-03-29 16:56:44 +0530

    Removed unsed variables

--- models/CategoryList/index.js
+++ models/CategoryList/index.js
@@ -4,15 +4,7 @@ const categoryListSchema = mongoose.Schema( {
   name      : {
     type     : String,
     default : ""
-  },
-  description : {
-    type    : String,
-    default : ""
-  },
-  is_active    : {
-    type     : Boolean,
-    default : true
-  },
+  }
 }, {collection: 'category_list'});
 
 module.exports = mongoose.model( "category_list", categoryListSchema );
