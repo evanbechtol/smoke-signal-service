14307b9da3a32d24e6aa8064bc887254f937538f
eevabec <evan.bechtol@ericsson.com>
2019-04-05 13:46:57 -0500

    Update Slack Message

--- util/slackUtil/index.js
+++ util/slackUtil/index.js
@@ -2,13 +2,27 @@ const logger = require( "../../config/logger" );
 
 function getTemplate ( data ) {
   try {
-    return `A cord has been ${( data.action ) ? "pulled" : "updated"}!\n` +
-        `- *User*: ${data.username}\n` +
+    const text = `- *User*: ${data.username}\n` +
         `- *Application*: ${data.app} \n` +
-        `- *Title*: ${data.title}  \n ` +
+        `- *Title*: ${data.title}  \n` +
         `- *Description*: ${data.description} \n` +
-        `- *Category*: ${data.category} \n\n` +
-        `_*<${data.url}|Click here for more details>*, and be a hero now!_`;
+        `- *Category*: ${data.category}`;
+
+    const attachments = {
+      fallback : `A cord has been ${( data.action ) ? "pulled" : "updated"}! _*<${data.url}|Click here for more details>*_`,
+      pretext  : `A cord has been ${( data.action ) ? "pulled" : "updated"}! _*<${data.url}|Click here for more details>*_`,
+      color    : "#0084F0", // Can either be one of 'good', 'warning', 'danger', or any hex color code
+      // Fields are displayed in a table on the message
+      fields   : [
+        {
+          title : data.title, // The title may not contain markup and will be escaped for you
+          value : text,
+          short : false // Optional flag indicating whether the `value` is short enough to be displayed side-by-side with other values
+        }
+      ]
+    };
+
+    return { text, attachments };
   } catch ( err ) {
     logger.error( `Error sending slack notification template: ${err}` );
   }
