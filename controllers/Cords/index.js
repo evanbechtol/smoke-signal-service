2be4048779c4f5f3b07d655f2638699b62422d89
eevabec <evan.bechtol@ericsson.com>
2019-04-10 08:03:30 -0500

    Lint Code & Error Handling
    - Add error handling where it was missing in the Cord controller
    - Add default error handler for Express
    - Lint code

--- controllers/Cords/index.js
+++ controllers/Cords/index.js
@@ -1,21 +1,21 @@
-const Cords                 = require( "../../models/Cords" );
-const CategoryList          = require( "../../models/CategoryList/index.js" );
-const qUtil                 = require( "../../util/queryUtil" );
-const resUtil               = require( "../../util/responseUtil" );
-const objectUtil            = require( "../../util" );
-const logger                = require( "../../config/logger" );
-const path                  = require( "path" );
-const fs                    = require( "fs" );
-const loki                  = require( "lokijs" );
-const dbName                = "db.json";
-const collectionName        = "files";
-const uploadPath            = "uploads";
-const db                    = new loki( `${uploadPath}/${dbName}`, { persistenceMethod : "fs" } );
-const config                = require( "../../config" );
-const slack                 = ( config.slackWebhookUrl !== undefined ) ? require( "slack-notify" )( `${config.slackWebhookUrl}` ) : false;
-const slackNotificationUtil = require( "../../util/slackUtil" );
+const Cords                  = require( "../../models/Cords" );
+const CategoryList           = require( "../../models/CategoryList/index.js" );
+const qUtil                  = require( "../../util/queryUtil" );
+const resUtil                = require( "../../util/responseUtil" );
+const objectUtil             = require( "../../util" );
+const logger                 = require( "../../config/logger" );
+const path                   = require( "path" );
+const fs                     = require( "fs" );
+const loki                   = require( "lokijs" );
+const dbName                 = "db.json";
+const collectionName         = "files";
+const uploadPath             = "uploads";
+const db                     = new loki( `${uploadPath}/${dbName}`, { persistenceMethod : "fs" } );
+const config                 = require( "../../config" );
+const slack                  = ( config.slackWebhookUrl !== undefined ) ? require( "slack-notify" )( `${config.slackWebhookUrl}` ) : false;
+const slackNotificationUtil  = require( "../../util/slackUtil" );
 const notificationController = require( "../../controllers/Notifications" );
-const UserApps = require( "../../models/userApps" );
+const UserApps               = require( "../../models/userApps" );
 
 const cordsKeyWhitelist = [
   "status",
@@ -46,21 +46,22 @@ module.exports = {
   getCategoryList
 };
 
-function getCords( req, res ) {
-  const queryStrings = qUtil.getDbQueryStrings(req.query);
+function getCords ( req, res ) {
+  const queryStrings = qUtil.getDbQueryStrings( req.query );
 
   Cords
-    .find(queryStrings.query)
-    .select({ __v: 0, description: 0 })
-    .sort(queryStrings.sort)
-    .limit(queryStrings.limit)
-    .exec(function (err, results) {
-      if (err) {
-        return res.status(500).send(resUtil.sendError(err));
-      }
+      .find( queryStrings.query )
+      .select( { __v : 0, description : 0 } )
+      .sort( queryStrings.sort )
+      .limit( queryStrings.limit )
+      .exec( function ( err, results ) {
+        if ( err ) {
+          logger.error( err );
+          return res.status( 500 ).send( resUtil.sendError( err ) );
+        }
 
-      return res.send(resUtil.sendSuccess(results));
-    });
+        return res.send( resUtil.sendSuccess( results ) );
+      } );
 }
 
 function getCategoryList ( req, res ) {
@@ -78,6 +79,7 @@ function getCategoryList ( req, res ) {
         return res.send( resUtil.sendSuccess( results ) );
       } )
       .catch( err => {
+        logger.error( err );
         return res.status( 500 ).send( resUtil.sendError( err ) );
       } );
 }
@@ -90,121 +92,128 @@ function getCordById ( req, res ) {
         return res.send( resUtil.sendSuccess( results ) );
       } )
       .catch( err => {
+        logger.error( err );
         return res.status( 500 ).send( resUtil.sendError( err ) );
       } );
 }
 
-function getCordByStatus( req, res ) {
+function getCordByStatus ( req, res ) {
   const status = req.query.status || req.params.status || null;
   Cords
-    .find({ status })
-    .sort({ openedOn: -1 })
-    .exec(function (err, results) {
-      if (err) {
-        return res.status(500).send(resUtil.sendError(err));
-      }
+      .find( { status } )
+      .sort( { openedOn : -1 } )
+      .exec( function ( err, results ) {
+        if ( err ) {
+          logger.error( err );
+          return res.status( 500 ).send( resUtil.sendError( err ) );
+        }
 
-      return res.send(resUtil.sendSuccess(results));
-    });
+        return res.send( resUtil.sendSuccess( results ) );
+      } );
 }
 
-function getCordForUser(req, res) {
-  let user = req.query.user || req.params.user || null;
+function getCordForUser ( req, res ) {
+  let user   = req.query.user || req.params.user || null;
   let status = req.query.status || req.params.status || "Open";
 
   try {
-    user = JSON.parse(user);
-  } catch (e) {
-    logger.error(`Error parsing user object: ${user}`);
-    return res.status(500).send(resUtil.sendError("Invalid JSON object provided"));
+    user = JSON.parse( user );
+  } catch ( e ) {
+    logger.error( `Error parsing user object: ${user}` );
+    return res.status( 500 ).send( resUtil.sendError( "Invalid JSON object provided" ) );
   }
 
   Cords
-    .find({ puller: user, status }, { __v: 0 })
-    .sort({ openedOn: -1 })
-    .exec(function (err, results) {
-      if (err) {
-        return res.status(500).send(resUtil.sendError(err));
-      }
+      .find( { puller : user, status }, { __v : 0 } )
+      .sort( { openedOn : -1 } )
+      .exec( function ( err, results ) {
+        if ( err ) {
+          logger.error( err );
+          return res.status( 500 ).send( resUtil.sendError( err ) );
+        }
 
-      return res.send(resUtil.sendSuccess(results));
-    });
+        return res.send( resUtil.sendSuccess( results ) );
+      } );
 }
 
-function getUserStats(req, res) {
+function getUserStats ( req, res ) {
   let user = req.query.user || req.params.user || null;
 
   try {
-    user = JSON.parse(user);
-  } catch (e) {
-    logger.error(`Error parsing user object: ${user}`);
-    return res.status(500).send(resUtil.sendError("Invalid JSON object provided"));
+    user = JSON.parse( user );
+  } catch ( e ) {
+    logger.error( `Error parsing user object: ${user}` );
+    return res.status( 500 ).send( resUtil.sendError( "Invalid JSON object provided" ) );
   }
   let stats = {};
 
   Cords
-    .count({ puller: user })
-    .then(cordCount => {
-      stats.cordsPulled = cordCount;
-      return Cords.count({ rescuers: user });
-    })
-    .then(rescuedCount => {
-      stats.rescuesProvided = rescuedCount;
-      return Cords.aggregate([
-        { $match: { puller: user } },
-        { $group: { _id: "$app", count: { $sum: 1 } } },
-        { $sort: { count: -1 } },
-        { $limit: 1 }
-      ]);
-    })
-    .then(appArray => {
-      stats.mostActiveApp = appArray[0];
-      return res.send(resUtil.sendSuccess(stats));
-    })
-    .catch(err => {
-      return res.status(500).send(resUtil.sendError(err));
-    });
+      .count( { puller : user } )
+      .then( cordCount => {
+        stats.cordsPulled = cordCount;
+        return Cords.count( { rescuers : user } );
+      } )
+      .then( rescuedCount => {
+        stats.rescuesProvided = rescuedCount;
+        return Cords.aggregate( [
+          { $match : { puller : user } },
+          { $group : { _id : "$app", count : { $sum : 1 } } },
+          { $sort : { count : -1 } },
+          { $limit : 1 }
+        ] );
+      } )
+      .then( appArray => {
+        stats.mostActiveApp = appArray[ 0 ];
+        return res.send( resUtil.sendSuccess( stats ) );
+      } )
+      .catch( err => {
+        logger.error( err );
+        return res.status( 500 ).send( resUtil.sendError( err ) );
+      } );
 }
 
 // Todo: create callback to send notification when cord created
-function createCord(req, res) {
-  if (req.body) {
-    const body = objectUtil.whitelist(req.body, cordsKeyWhitelist);
+function createCord ( req, res ) {
+  if ( req.body ) {
+    const body = objectUtil.whitelist( req.body, cordsKeyWhitelist );
     Cords
-      .create(body, function (err, results) {
-        if (err) {
-          return res.status(500).send(resUtil.sendError(err));
-        }
-		
-		UserApps.find({
-      apps: results.app,
-      "user.username": { $ne: results.puller.username }
-    })
-      .select({ __v: 0, description: 0 })
-      .exec(function(err, resp) {
-        if (err) {
-          return err;
-        }
-        sendSlackNotifications( req, true );
-        results.subject = "New Cord has been created";
-        notificationController
-          .createNotification(results, resp)
-          .then(respData => {
-            return res.send(resUtil.sendSuccess(respData));
-          })
-          .catch(err => {
-            return res.send(err);
-          });
-      });
-		
-      });
+        .create( body, function ( err, results ) {
+          if ( err ) {
+            logger.error( err );
+            return res.status( 500 ).send( resUtil.sendError( err ) );
+          }
+
+          UserApps.find( {
+            apps            : results.app,
+            "user.username" : { $ne : results.puller.username }
+          } )
+              .select( { __v : 0, description : 0 } )
+              .exec( function ( err, resp ) {
+                if ( err ) {
+                  logger.error( err );
+                  return err;
+                }
+                sendSlackNotifications( req, true );
+                results.subject = "New Cord has been created";
+                notificationController
+                    .createNotification( results, resp )
+                    .then( respData => {
+                      return res.send( resUtil.sendSuccess( respData ) );
+                    } )
+                    .catch( err => {
+                      logger.error( err );
+                      return res.send( err );
+                    } );
+              } );
+
+        } );
   } else {
-    return res.status(400).send(resUtil.sendError("Request body not provided"));
+    return res.status( 400 ).send( resUtil.sendError( "Request body not provided" ) );
   }
 }
 
-function updateCord(req, res) {
-  const _id = req.query.id || req.params.id || null;
+function updateCord ( req, res ) {
+  const _id                 = req.query.id || req.params.id || null;
   const updateCordWhitelist = [
     "status",
     "description",
@@ -216,82 +225,86 @@ function updateCord(req, res) {
     "title",
     "tags"
   ];
-  if (_id && req.body) {
-    const body = objectUtil.whitelist(req.body, updateCordWhitelist);
+  if ( _id && req.body ) {
+    const body = objectUtil.whitelist( req.body, updateCordWhitelist );
     Cords
-      .findByIdAndUpdate(_id, body, { new: true })
-      .exec(function (err, results) {
-        if (err) {
-          return res.status(500).send(resUtil.sendError(err));
-        }
-		sendSlackNotifications( req, false );
-        notificationController.userDiscussion(results).
-        then(resp=>{
-          return res.send(resUtil.sendSuccess(resp));
-        }).catch(err => {
-          throw err;
-        });
+        .findByIdAndUpdate( _id, body, { new : true } )
+        .exec( function ( err, results ) {
+          if ( err ) {
+            return res.status( 500 ).send( resUtil.sendError( err ) );
+          }
+          sendSlackNotifications( req, false );
+          notificationController.userDiscussion( results ).then( resp => {
+            return res.send( resUtil.sendSuccess( resp ) );
+          } ).catch( err => {
+            logger.error( err );
+            return res.status( 500 ).send( resUtil.sendError( err ) );
+          } );
 
-      });
+        } );
   } else {
-    return res.status(400).send(resUtil.sendError("Request ID or Body was not provided"));
+    return res.status( 400 ).send( resUtil.sendError( "Request ID or Body was not provided" ) );
   }
 }
 
-function updateRescuers(req, res) {
-  const _id = req.query.id || req.params.id || null;
-  const updateCordWhitelist = ["rescuers"];
-  if (_id && req.body && req.body.rescuers) {
-    const body = objectUtil.whitelist(req.body, updateCordWhitelist);
+function updateRescuers ( req, res ) {
+  const _id                 = req.query.id || req.params.id || null;
+  const updateCordWhitelist = [ "rescuers" ];
+  if ( _id && req.body && req.body.rescuers ) {
+    const body = objectUtil.whitelist( req.body, updateCordWhitelist );
     Cords
-      .findByIdAndUpdate(_id, { $addToSet: { "rescuers": { $each: body.rescuers } } }, { new: true })
-      .then(results => {
-        if (!results) {
-          return res.status(404).send(resUtil.sendError("Document not found matching provided ID"));
-        }
-        return res.send(resUtil.sendSuccess(results));
-      })
-      .catch(err => {
-        return res.status(500).send(resUtil.sendError(err));
-      });
+        .findByIdAndUpdate( _id, { $addToSet : { "rescuers" : { $each : body.rescuers } } }, { new : true } )
+        .then( results => {
+          if ( !results ) {
+            return res.status( 404 ).send( resUtil.sendError( "Document not found matching provided ID" ) );
+          }
+          return res.send( resUtil.sendSuccess( results ) );
+        } )
+        .catch( err => {
+          logger.error( err );
+          return res.status( 500 ).send( resUtil.sendError( err ) );
+        } );
   } else {
-    return res.status(400).send(resUtil.sendError("Request ID or Body was not provided"));
+    return res.status( 400 ).send( resUtil.sendError( "Request ID or Body was not provided" ) );
   }
 }
 
-async function upload(req, res) {
+async function upload ( req, res ) {
   const _id = req.query.id || req.params.id || null;
-  if (_id) {
+  if ( _id ) {
     try {
-      const col = await objectUtil.loadCollection(collectionName, db);
-      const data = col.insert(req.file);
+      const col  = await objectUtil.loadCollection( collectionName, db );
+      const data = col.insert( req.file );
 
       db.saveDatabase();
-      if (data && data.filename) {
-        Cords.findByIdAndUpdate(_id, { "files": data.filename }, { new: true })
-          .then(response => {
-            return res.send(response);
-          })
-          .catch(err => {
-            throw err;
-          });
+      if ( data && data.filename ) {
+        Cords.findByIdAndUpdate( _id, { "files" : data.filename }, { new : true } )
+            .then( response => {
+              return res.send( response );
+            } )
+            .catch( err => {
+              logger.error( err );
+              return res.status( 500 ).send( resUtil.sendError( err ) );
+            } );
       } else {
-        return res.status(500).send(resUtil.sendError("Error uploading file, please try again"));
+        logger.error( "Error uploading file, please try again" );
+        return res.status( 500 ).send( resUtil.sendError( "Error uploading file, please try again" ) );
       }
 
-    } catch (err) {
-      return res.status(400).send(resUtil.sendError(err));
+    } catch ( err ) {
+      logger.error( err );
+      return res.status( 400 ).send( resUtil.sendError( err ) );
     }
   } else {
-    return res.status(400).send(resUtil.sendError("Invalid request: ID is required"));
+    return res.status( 400 ).send( resUtil.sendError( "Invalid request: ID is required" ) );
   }
 }
 
-async function getFilesByCordId(req, res) {
+async function getFilesByCordId ( req, res ) {
   const _id = req.query.id || req.params.id || null;
-  const col = await objectUtil.loadCollection(collectionName, db);
+  const col = await objectUtil.loadCollection( collectionName, db );
 
-  if (_id) {
+  if ( _id ) {
     try {
       Cords.findById( _id, { files : 1 } )
           .then( response => {
@@ -307,22 +320,24 @@ async function getFilesByCordId(req, res) {
             return res.send( resUtil.sendSuccess( result ) );
           } )
           .catch( err => {
+            logger.error( err );
             throw err;
           } );
     } catch ( err ) {
+      logger.error( err );
       return res.status( 400 ).send( resUtil.sendError( err ) );
     }
   } else {
-    return res.status(400).send(resUtil.sendError("Invalid request: ID is required"));
+    return res.status( 400 ).send( resUtil.sendError( "Invalid request: ID is required" ) );
   }
 }
 
-async function getFile(req, res) {
+async function getFile ( req, res ) {
   const fileName = req.query.id || req.params.id || null;
 
-  const col      = await objectUtil.loadCollection( collectionName, db );
+  const col = await objectUtil.loadCollection( collectionName, db );
 
-  if (fileName) {
+  if ( fileName ) {
     try {
       const result = col.findOne( { "filename" : fileName } );
 
@@ -333,28 +348,30 @@ async function getFile(req, res) {
       res.setHeader( "Content-Type", result.mimetype );
       fs.createReadStream( path.join( uploadPath, result.filename ) ).pipe( res );
     } catch ( err ) {
+      logger.error( err );
       return res.status( 400 ).send( resUtil.sendError( err ) );
     }
   } else {
-    return res.status(400).send(resUtil.sendError("Invalid request: ID is required"));
+    return res.status( 400 ).send( resUtil.sendError( "Invalid request: ID is required" ) );
   }
 }
 
-function deleteCord(req, res) {
+function deleteCord ( req, res ) {
   const _id = req.query.id || req.params.id || null;
 
-  if (_id) {
+  if ( _id ) {
     Cords
-      .findByIdAndDelete(_id)
-      .exec(function (err, results) {
-        if (err) {
-          return res.status(500).send(resUtil.sendError(err));
-        }
+        .findByIdAndDelete( _id )
+        .exec( function ( err, results ) {
+          if ( err ) {
+            logger.error( err );
+            return res.status( 500 ).send( resUtil.sendError( err ) );
+          }
 
-        return res.send(resUtil.sendSuccess(results));
-      });
+          return res.send( resUtil.sendSuccess( results ) );
+        } );
   } else {
-    return res.status(400).send(resUtil.sendError("Request ID was not provided"));
+    return res.status( 400 ).send( resUtil.sendError( "Request ID was not provided" ) );
   }
 }
 
--- controllers/UserApps/index.js
+++ controllers/UserApps/index.js
@@ -1,23 +1,26 @@
-const resUtil = require( "../../util/responseUtil" );
-const objectUtil = require( "../../util" );
-const UserApps = require( "../../models/userApps" );
-const userAppsKeyWhitelist = ["user", "apps"];
+const resUtil              = require( "../../util/responseUtil" );
+const objectUtil           = require( "../../util" );
+const UserApps             = require( "../../models/userApps" );
+const logger               = require( "../../config/logger" );
+const userAppsKeyWhitelist = [ "user", "apps" ];
 
 module.exports = {
   createUserApps
 };
 
 // Todo: create callback to create UserApps when user registered
-function createUserApps( req, res ) {
+function createUserApps ( req, res ) {
   if ( req.body ) {
     const body = objectUtil.whitelist( req.body, userAppsKeyWhitelist );
-    UserApps.create(body, function( err, results ) {
-      if (err) {
-        return res.status(500).send(resUtil.sendError(err));
+    UserApps.create( body, function ( err, results ) {
+      if ( err ) {
+        logger.error( err );
+        return res.status( 500 ).send( resUtil.sendError( err ) );
       }
-      return res.send(resUtil.sendSuccess(results));
-    });
+      return res.send( resUtil.sendSuccess( results ) );
+    } );
   } else {
-    return res.status( 400 ).send(resUtil.sendError( "Request body not provided" ));
+    logger.error( "Request body not provided" );
+    return res.status( 400 ).send( resUtil.sendError( "Request body not provided" ) );
   }
 }
