645d0674cf3a1c9cb86ab3de496737d64f643db8
eevabec <evan.bechtol@ericsson.com>
2019-04-26 14:47:24 -0500

    Refactor ObjectService tests
    - Break multiple assertions into multiple tests

--- test/ObjectService/index.js
+++ test/ObjectService/index.js
@@ -6,39 +6,50 @@ const CordsKeyWhitelist = require( "../../config/keysWhitelists/cords" );
 
 mocha.describe( "Object Service", () => {
   const ObjectServiceInstance = new ObjectService();
+  let result;
 
-  it( "Instance should be created", () => {
-    assert.exists( ObjectServiceInstance );
+  context( "Create ObjectService Instance", () => {
+    it( "Instance is created", () => {
+      assert.exists( ObjectServiceInstance );
+    } );
+
+    it( "Has 'pick' method", () => {
+      assert.isFunction( ObjectService.pick );
+    } );
   } );
 
-  it( "Should have 'pick' method", () => {
-    assert.isFunction( ObjectService.pick );
-  } );
+  context( "Whitelist", () => {
+    it( "Returns data", () => {
+      const object = {
+        status: "Open",
+        description: "Here is a test description from the DB",
+        openedOn: "2019-02-19T20:57:00.009Z",
+        rescuers: [],
+        likes: 0,
+        tags: [],
+        puller: { "_id": 1, "username": "eevabec" },
+        _id: "5c6c7499fb6fc01c4ce7dfd2",
+        title: "500 Status code on login",
+        app: "Knowledge Catalog",
+        category: "Authentication",
+        someBadKey: "12345",
+        AnotherBadKey: {
+          nestedStuff: 123,
+          nestedArray: [ 1, 2, 3, "a" ]
+        }
+      };
 
-  it( "Should return only whitelisted keys", () => {
-    const object = {
-      status: "Open",
-      description: "Here is a test description from the DB",
-      openedOn: "2019-02-19T20:57:00.009Z",
-      rescuers: [],
-      likes: 0,
-      tags: [],
-      puller: { "_id": 1, "username": "eevabec" },
-      _id: "5c6c7499fb6fc01c4ce7dfd2",
-      title: "500 Status code on login",
-      app: "Knowledge Catalog",
-      category: "Authentication",
-      someBadKey: "12345",
-      AnotherBadKey: {
-        nestedStuff: 123,
-        nestedArray: [ 1, 2, 3, "a" ]
-      }
-    };
+      result = ObjectService.pick( object, CordsKeyWhitelist.post );
 
-    const result = ObjectService.pick( object, CordsKeyWhitelist.post );
+      assert.exists( result );
+    } );
 
-    assert.exists( result );
-    assert.isObject( result );
-    assert.hasAllKeys( result, CordsKeyWhitelist.post );
+    it( "Is an object", () => {
+      assert.isObject( result );
+    } );
+
+    it( "Has only whitelisted keys", () => {
+      assert.hasAllKeys( result, CordsKeyWhitelist.post );
+    } );
   } );
 } );
