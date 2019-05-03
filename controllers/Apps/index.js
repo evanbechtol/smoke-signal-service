3efde68fad7343411312ff77498491e4e5991941
eevabec <evan.bechtol@ericsson.com>
2019-05-03 11:43:55 -0500

    Refactor Apps Controller
    - Utilize AppService to abstract business logic out of controller

--- controllers/Apps/index.js
+++ controllers/Apps/index.js
@@ -1,22 +1,31 @@
+const AppService = require( "../../services/AppService" );
 const resUtil = require( "../../middlewares/response" );
-const qUtil   = require( "../../util/queryUtil" );
-const Apps    = require( "../../models/Apps" );
+const qUtil = require( "../../util/queryUtil" );
+const Apps = require( "../../models/Apps" );
 const logger = require( "../../services/Logger" );
+const Sentry = require( "@sentry/node" );
 
+// Create a usable instance of the Cord Service
+const AppServiceInstance = new AppService( Apps );
 
 module.exports = {
   getApps
 };
 
-function getApps ( req, res ) {
+async function getApps ( req, res ) {
   const queryStrings = qUtil.getDbQueryStrings( req.query );
-  Apps
-      .find( queryStrings.query )
-      .exec( function ( err, results ) {
-        if ( err ) {
-          logger.error( err );
-          return res.status( 500 ).send( resUtil.sendError( err ) );
-        }
-        return res.send( resUtil.sendSuccess( results ) );
-      } );
+
+  try {
+    const data = await AppServiceInstance.find( queryStrings.query );
+    return res.send( resUtil.sendSuccess( data ) );
+  } catch ( err ) {
+    logger.error( err );
+    res.status( 500 ).send( resUtil.sendError( err ) );
+
+    // Capture the error with user information provided
+    Sentry.withScope( scope => {
+      scope.setTag( "query", queryStrings.query );
+      Sentry.captureException( err );
+    } );
+  }
 }
