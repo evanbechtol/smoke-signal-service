0e8b7c6b77e7b68c2588cd2373fb1c1d17e5e801
eevabec <evan.bechtol@ericsson.com>
2019-05-03 08:20:08 -0500

    Change e-auth to devtools

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const config = {
   port: process.env.PORT || 3000,
   dbUrl: process.env.DBURL || `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@ds343895.mlab.com:43895/smoke-signal`,
-  eAuthUrl: process.env.EAUTHURL || "http://129.192.179.179",
+  eAuthUrl: process.env.EAUTHURL || "https://tools.nrws.ericsson.net",
   env: process.env.NODE_ENV || "development",
   logDir: process.env.LOGDIR || "logs",
   viewEngine: process.env.VIEW_ENGINE || "html",
