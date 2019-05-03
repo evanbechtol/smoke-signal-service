ceed17e7fc01349b9ec66add316adbdce9a7bedb
eevabec <evan.bechtol@ericsson.com>
2019-05-03 11:43:10 -0500

    Create AppService
    - Create to abstract business logic out of controller

---
+++ services/AppService.js
@@ -0,0 +1,24 @@
+const MongooseService = require( "./MongooseService" );
+
+class AppService {
+  /**
+   * @description Initializes a new instance of the AppService
+   * @param AppModel {mongoose.model} Required: Instance of Mongoose model
+   */
+  constructor ( AppModel ) {
+    this.cordModel = AppModel;
+    this.mongooseServiceInstance = new MongooseService( this.cordModel );
+  }
+
+  /**
+   * @description Get multiple documents by ID
+   * @param query {object} Required: Query to execute on Model
+   * @param {object} [sort] Optional: argument to sort data
+   * @returns {Promise} Returns result of Mongoose query
+   */
+  async find ( query, sort ) {
+    return await this.mongooseServiceInstance.find( query, { __v: 0 }, sort );
+  }
+}
+
+module.exports = AppService;
